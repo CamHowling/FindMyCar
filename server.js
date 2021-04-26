a2b6fc423b1fc2a4e8093b9e7265fe97f2bb1612
wolfsmind <cameronch1993@gmail.com>
2021-04-27 02:10:29 +1000

    added entire project

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.set('view engine', 'ejs'); //ejs for templates - may replace with pug templa
 //app.use(express.static(__dirname + '/assets')); //need to re-establish after SQL template used
 
 app.use(session({
-	secret: 'vidyapathaisalwaysrunning', //rename?
+	secret: 'findmycar', //rename?
 	resave: true,
 	saveUninitialized: true
 })); // session secret?
@@ -58,58 +58,3 @@ require('./database/dbroutes.js')(app, passport); //load routes and pass in app 
 http.listen(port,()=>{
   console.log("Listening on port ", port);
 });
-
-
-/*
-//express and pug stuff
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.get('/carspace/:id', retrieveCarSpace);
-app.get('/carspace', retrieveCarSpaces);
-
-//fetch requests http will need modification
-
-
-app.post('/user', function (req, res) {
-  fetch('http://api-server-hostname:api-server-port/user', {
-      method: 'POST',
-      body: req.body,
-  }).json(json => {
-      res.render('/user/user.pug', json)
-  }).error(error => {
-      res.render('/error/error.pug', { error: error })
-  });
-})
-
-app.get('/user/:id-:firstname', function (req, res) {
-  fetch('http://api-server-hostname:api-server-port/user/' + req.params['id'], {
-      method: 'GET',
-      body: user,
-  }).json(json => {
-      res.render('/user/user.pug', json)
-  }).error(error => {
-      res.render('/error/error.pug', { error: error })
-  })
-})
-*/
-
-/*
-//socket test
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  setInterval(()=>{
-    socket.emit('number', parseInt(Math.random()*10));
-  }, 1000);
-
-});
-*/
-
-//this is only needed for Cloud foundry 
-//require("cf-deployment-tracker-client").track();
-
-
-//notes about this file
-//basic server express node.js file, some pieces may not be needed
